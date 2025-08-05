0902efca7387e207bcb9237234ac5dd33acf48ee
andreasmz <174248270+andreasmz@users.noreply.github.com>
2025-08-05 15:59:32 +0200

    rewritten init and main files

--- neurotorchmz/__init__.py
+++ neurotorchmz/__init__.py
@@ -1,27 +1,38 @@
-""" Neurotorch is a tool designed to extract regions of synaptic activity in neurons tagges with iGluSnFR, but is in general capable to find any kind of local brightness increase due to synaptic activity  """
-__version__ = "25.7.1"
+""" 
+Neurotorch is a tool designed to extract regions of synaptic activity in neurons tagges with iGluSnFR, but is in general capable to find any kind of local brightness increase 
+due to synaptic activity.
+"""
+__version__ = "25.8.1"
 __author__ = "Andreas Brilka"
 
 from .core.session import Session, Edition
-from .utils.api import API
+from .core import api as API # pyright: ignore[reportUnusedImport]
 
-def Start(edition: Edition = Edition.NEUROTORCH, headless: bool = False, background: bool = False) -> Session:
+def start(edition: Edition = Edition.NEUROTORCH, headless: bool = False, background: bool = False) -> Session:
     """ 
         Create a new session of Neurotorch. You can access it via the provided Session object returned by this function (only in background mode).
-
-        In contrast to a session, use the API if you don't want Neurotorch to manage your data and only want to access the detection functions.
+        In contrast to a session, use neurotorchmz.API if you don't want Neurotorch to manage your data and only want to access the detection functions.
     
         :param Edition edition: Choose which edition of Neurotorch you want to launch
         :param bool headless: When set to true, the tkinter GUI is not opened. Some functions may not work in headless mode
         :param bool background: Controls wether the GUI is running in a different thread. Note that tkinter may raise warnings, which can generally be ignored.
+        :returns Session: A session object to interact with the GUI
     """
     session = Session(edition=edition)
     if not headless:
         session.launch(background=background)
     return session
 
-def Start_Background(edition:Edition = Edition.NEUROTORCH, headless=False) -> Session:
+def start_background(edition:Edition = Edition.NEUROTORCH, headless: bool = False) -> Session:
     """ 
-        Create a new session of NeurotorchMZ. The same as calling Start with background=True
+        Create a new session of Neurotorch. You can access it via the provided Session object returned by this function (only in background mode).
+        In contrast to a session, use neurotorchmz.API if you don't want Neurotorch to manage your data and only want to access the detection functions.
+        
+        Note: This function is an alias for start(background=True)
+
+        :param Edition edition: Choose which edition of Neurotorch you want to launch
+        :param bool headless: When set to true, the tkinter GUI is not opened. Some functions may not work in headless mode
+        :param bool background: Controls wether the GUI is running in a different thread. Note that tkinter may raise warnings, which can generally be ignored.
+        :returns Session: A session object to interact with the GUI
     """
-    return Start(edition=edition, headless=headless, background=True) 
+    return start(edition=edition, headless=headless, background=True) 
--- neurotorchmz/__main__.py
+++ neurotorchmz/__main__.py
@@ -1,7 +1,7 @@
-from . import Start, Edition
+from . import start, Edition
 import sys
 if __name__ == "__main__":
     if "NEUROTORCH_DEBUG" in sys.argv:
-        Start(Edition.NEUROTORCH_DEBUG)
+        start(Edition.NEUROTORCH_DEBUG)
     else:
-        Start()
+        start()
